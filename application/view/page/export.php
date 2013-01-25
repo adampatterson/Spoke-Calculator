986822689384c58a657c2c92d74186d45db04570
Adam Patterson <hello@adampatterson.ca>
2013-01-25 16:37:30 -0700

    Deleted old files

--- application/view/page/export.php
+++
@@ -1,7 +0,0 @@
-<? load::view('partials/header', array( 'title'=>'Export' )); ?>
-<div class="column span-21 last content-section">
-<strong>Edport</strong> <a href="<?=url::page("index.php/export/sql/hub"); ?>">Hubs</a>
-<br/>
-<strong>Export</strong> <a href="<?=url::page("index.php/export/sql/rim"); ?>">Rims</a>
-</div>
-<? load::view('partials/footer'); ?>
