71463803272fc7531d781b37d27b6f18354592ee
Adam Patterson <hello@adampatterson.ca>
2013-01-27 01:14:05 -0700

    Rim and Hub query cleanup

--- application/model/hub.php
+++ application/model/hub.php
@@ -5,22 +5,9 @@ class hub_model
 
 	public function get( $frontrear='', $string='' )
 	{
-        $raw_string = explode(' ', $string);
-        $raw_total = count($raw_string);
-
-        $name = $raw_string[0];
-
-        if ( $raw_total == 1 )
-        {
-            return db::query("SELECT * FROM hubs WHERE `frontrear` = '$frontrear' AND `name` LIKE '%$name%'");
-        } else {
-
-            $model = ltrim(str_replace($name, '', $string));
-
-            $model = make_for_search( $model );
-
-            return db::query("SELECT * FROM hubs WHERE `frontrear` = '$frontrear' AND `name` LIKE '%$name%' AND `model` LIKE '%$model%'");
-        }
+		$string = make_for_search( $string );
+		
+        return db::query("SELECT * FROM hubs WHERE `frontrear` = '$frontrear' AND `name` LIKE '%$string%'");
 	}
 	
 	
--- application/model/rim.php
+++ application/model/rim.php
@@ -4,22 +4,9 @@ class rim_model
 {
 	public function get( $size='', $string = '' )
 	{
-        $raw_string = explode(' ', $string);
-        $raw_total = count($raw_string);
-
-        $name = $raw_string[0];
-
-        if ( $raw_total == 1 )
-        {
-            return db::query("SELECT * FROM rims WHERE `size` = '$size' AND `name` LIKE '%$name%'");
-        } else {
-
-            $model = ltrim(str_replace($name, '', $string));
-
-            $model = make_for_search( $model );
-
-            return db::query("SELECT * FROM rims WHERE `size` = '$size' AND `name` LIKE '%$name%' AND `model` LIKE '%$model%'");
-        }
+	    $string = make_for_search( $string );
+	
+		return db::query("SELECT * FROM rims WHERE `size` = '$size' AND `name` LIKE '%$string%'");
 
 	}
 	
