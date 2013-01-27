24fce7687760ad8f1ab7bbda729b503acb9469a3
Adam Patterson <hello@adampatterson.ca>
2013-01-27 16:33:36 -0700

    Click events on items, Loading data() into it

--- application/view/page/home.php
+++ application/view/page/home.php
@@ -3,8 +3,23 @@
 <script type="text/javascript" charset="utf-8">
 
 	$(document).ready(function(){
+
         $('#hub_search').keyup(get_hubs);
         $('#rim_search').keyup(get_rims);
+
+        $(".rim_data").live("click",function() {
+
+            var name = $(this).data('name');
+
+            console.log("clicked on "+ name);
+        });
+
+        $(".hub_data").live("click",function() {
+
+            var name = $(this).data('name');
+
+            console.log("clicked on "+ name);
+        });
 
 	});
 
@@ -38,8 +53,19 @@
                         $.getJSON(url,
                             function(data){
 
-                                $.each(data.slice(0,10), function(i, item){
-                                    $(".hub_results").append('<li>' + item.name +' '+ item.model + '</li>');
+                                $.each(data.slice(0,5), function(i, item){
+
+                                    $html ='<div class="hub_data span3" data-name="' + item.name +'"><strong>' + item.name +'</strong>' +
+                                            '<ul>' +
+                                                '<li>Left flange ø: '+ item.fdl +' mm</li>' +
+                                                '<li>Right flange ø: '+ item.fdr +' mm</li>' +
+                                                '<li>Centre to left flange: '+ item.cfl +' mm</li>' +
+                                                '<li>Centre to right flange: '+ item.cfr +' mm</li>' +
+                                                '<li>Spoke hole ø: '+ item.spo +' mm</li>' +
+                                            '</ul></div>';
+
+                                    $(".hub_results").append($html);
+
                                 });
                             });
 					}
@@ -79,8 +105,17 @@
                         $.getJSON(url,
                             function(data){
 
-                                $.each(data.slice(0,10), function(i, item){
-                                    $(".rim_results").append('<li>' + item.name +' '+ item.model + '</li>');
+                                $.each(data.slice(0,5), function(i, item){
+                                   
+ 										$html ='<div class="rim_data span3" data-name="' + item.name +'"><strong>' + item.name +'</strong>' +
+                                            '<ul>' +
+                                                '<li>ERD: '+ item.erd +' mm</li>' +
+                                                '<li>OSB: '+ item.osb +' mm</li>' +
+                                                '<li>Size: '+ item.size +'</li>' +
+                                            '</ul></div>';
+
+                                    $(".rim_results").append($html);                                    
+
                                 });
                             });
                     }
@@ -88,6 +123,7 @@
             });
 
         }
+
     }
 </script>
 
@@ -96,68 +132,88 @@
 		<div class="span12">
 			<p class="lead">The
 				<input type='radio' value="front" data-front-rear="front" name="frontrear" checked class="front_rear">Front <input type='radio' value="rear" data-front-rear="rear" name="frontrear" class="front_rear">Rear wheel has
-				<input type="text" class="" placeholder="32" name="spokes" required> spokes,
-				<input type="text" class="" placeholder="3" name="cross" required> cross and is
+				<input type="text" class="" placeholder="32" value="32" name="spokes" required> spokes,
+				<input type="text" class="" placeholder="3" value="3" name="cross" required> cross and is
 				<input type="text" class="" placeholder="26" value='26' name="rsize" required> big.</p>
 		</div>
-		<div class="span12">
-			<h3>Existing</h3>
+
+        <div class="span12">
+
 			<div class="row">
-				<div class="span1"><strong>Rim</strong>:</div>
-				<div class="span4">
-					<input class="span3" id="rim_search"type="text" placeholder="">
-					<div class="rim_results"></div>
-				</div>
-				<div class="span1"><strong>Hub</strong>:</div>
-				<div class="span4">
-					<input class="span3" id="hub_search" type="text" placeholder="">
-					<div class="hub_results"></div>
-				</div>
+
+                <div class="span4 ">
+
+                    <h3>Measurements</h3>
+
+                    <table cellpadding="0" cellspacing="0">
+                        <tbody>
+                        <tr>
+                            <td></td>
+                            <td align="center"><div class="sc_label">left</div></td>
+                            <td></td>
+                            <td align="center"><div class="sc_label">right</div></td>
+                            <td></td>
+                        </tr>
+                        <tr>
+                            <td>Effective rim diameter <strong>(ERD)</strong>:&nbsp;</td>
+                            <td><input name="erd" value="" type="text" class="span1"></td>
+                            <td colspan="3">&nbsp;mm</td>
+                        </tr>
+                        <tr>
+                            <td>Flange diamiter <strong>(FD)</strong>:&nbsp;</td>
+                            <td><input name="fdl" value="" type="text" class="span1"></td>
+                            <td>&nbsp;mm&nbsp;&nbsp;</td>
+                            <td><input name="fdr" value="" type="text" class="span1"></td>
+                            <td>&nbsp;mm</td>
+                        </tr>
+                        <tr>
+                            <td>Center to Flange <strong>(CF)</strong>:&nbsp;</td>
+                            <td><input name="cfl" value="" type="text" class="span1"></td>
+                            <td>&nbsp;mm&nbsp;&nbsp;</td>
+                            <td><input name="cfr" value="" type="text" class="span1"></td>
+                            <td>&nbsp;mm</td>
+                        </tr>
+                        </tbody>
+                    </table>
+
+                </div>
+
+                <div class="span4">
+
+                    <h3>Existing</h3>
+
+                    <strong>Rim</strong>:
+                    <input class="span3" id="rim_search"type="text" placeholder="">
+                    <div class="rim_results"></div>
+
+                </div>
+
+                <div class="span4">
+
+                    <h3>&nbsp;</h3>
+
+                    <strong>Hub</strong>:
+                    <input class="span3" id="hub_search" type="text" placeholder="">
+                    <div class="hub_results"></div>
+
+                </div>
+
 			</div>
+
+            <div class="row">
+
+                <div class="span8 offset4">
+                    <div class="form-actions">
+                        <button name="values" class="btn btn-primary">Load Values</button>
+                    </div>
+                </div>
+
+            </div>
+
 		</div>
-		<div class="span12">
-			<div class="form-actions">
-				<button name="values" class="btn btn-primary">Load Values</button>
-			</div>
-		</div>
-		<div class="span12">
-			<h3>Measurements</h3>
-			<table cellpadding="0" cellspacing="0">
-				<tbody>
-					<tr>
-						<td></td>
-						<td align="center"><div class="sc_label">left</div></td>
-						<td></td>
-						<td align="center"><div class="sc_label">right</div></td>
-						<td></td>
-					</tr>
-					<tr>
-						<td>Effective rim diameter <strong>(ERD)</strong>:&nbsp;</td>
-						<td><input name="erd" value="" type="text" class="span1"></td>
-						<td colspan="3">&nbsp;mm</td>
-					</tr>
-					<tr>
-						<td>Flange diamiter <strong>(FD)</strong>:&nbsp;</td>
-						<td><input name="fdl" value="" type="text" class="span1"></td>
-						<td>&nbsp;mm&nbsp;&nbsp;</td>
-						<td><input name="fdr" value="" type="text" class="span1"></td>
-						<td>&nbsp;mm</td>
-					</tr>
-					<tr>
-						<td>Center to Flange <strong>(CF)</strong>:&nbsp;</td>
-						<td><input name="cfl" value="" type="text" class="span1"></td>
-						<td>&nbsp;mm&nbsp;&nbsp;</td>
-						<td><input name="cfr" value="" type="text" class="span1"></td>
-						<td>&nbsp;mm</td>
-					</tr>
-				</tbody>
-			</table>
-		</div>
-		<div class="span12">
-			<div class="form-actions">
-				<input type="submit" name="Calculate" value="Calculate" class="btn btn-primary" tabindex="16" />
-			</div>
-		</div>
+
+
+
 	</form>
 </div>
 <? load::view('partials/footer'); ?>
